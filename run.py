5f9696da5da2a6dcb1a09a9b0724d45707d4ac16
wei.zhong <wei.zhong@lge.com>
2024-03-06 16:34:57 -0500

    ignore errors.

--- run.py
+++ run.py
@@ -264,8 +264,7 @@ def test(
     )
 
     for config_file in config_file_list:
-        if True:
-        #try:
+        try:
             render_helper = RenderHelper(
                 config_file, args.result_dir, args.action_set_tag
             )
@@ -371,15 +370,15 @@ def test(
                     Path(args.result_dir) / "traces" / f"{task_id}.zip"
                 )
 
-        #except Exception as e:
-        #    logger.info(f"[Unhandled Error] {repr(e)}]")
-        #    import traceback
+        except Exception as e:
+            logger.info(f"[Unhandled Error] {repr(e)}]")
+            import traceback
 
-        #    # write to error file
-        #    with open(Path(args.result_dir) / "error.txt", "a") as f:
-        #        f.write(f"[Config file]: {config_file}\n")
-        #        f.write(f"[Unhandled Error] {repr(e)}\n")
-        #        f.write(traceback.format_exc())  # write stack trace to file
+            # write to error file
+            with open(Path(args.result_dir) / "error.txt", "a") as f:
+                f.write(f"[Config file]: {config_file}\n")
+                f.write(f"[Unhandled Error] {repr(e)}\n")
+                f.write(traceback.format_exc())  # write stack trace to file
 
         render_helper.close()
 
