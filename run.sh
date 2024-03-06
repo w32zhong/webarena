835336c4e764a58bcc90f4774559726ba0f06164
wei.zhong <wei.zhong@lge.com>
2024-03-05 23:09:44 -0500

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 python scripts/generate_test_data.py
+python agent/prompts/to_json.py
 python run.py $@
 
 #python run.py --instruction_path agent/prompts/jsons/p_cot_id_actree_2s.json --test_start_idx 0 --test_end_idx 1 --result_dir output/ --model Zyphra/BlackMamba-2.8B --tokenizer EleutherAI/gpt-neox-20b
